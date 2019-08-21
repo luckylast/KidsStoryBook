5b28427d04dd42a830b27049f5396bb1323a2877
Han, Amy <ahan@illumina.com>
2019-08-21 16:58:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kidsstorybook/Config.java
@@ -0,0 +1,13 @@
+package com.example.kidsstorybook;
+
+/**
+ * Created by Amy on 21/08/19.
+ */
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyDl93LmW-gka8Z4Mr7L7k1FRm3N8WzF1XE";
+
+}
